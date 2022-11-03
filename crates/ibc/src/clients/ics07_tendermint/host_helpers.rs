1dd6856418ac164192497f43e309f1a46270b933
Philippe Laferriere <plafer@protonmail.com>
2022-11-03 10:51:32 -0400

    move host_helpers to tendermint client dir

--- crates/ibc/src/clients/ics07_tendermint/host_helpers.rs
+++ crates/ibc/src/clients/ics07_tendermint/host_helpers.rs
@@ -4,22 +4,19 @@ use alloc::format;
 use alloc::string::{String, ToString};
 use ibc_proto::google::protobuf::Any;
 
-use crate::Height;
 use crate::clients::ics07_tendermint::client_state::ClientState as TmClientState;
 use crate::core::ics02_client::client_state::ClientState;
 use crate::core::ics03_connection::error::Error;
 use crate::core::ics23_commitment::specs::ProofSpecs;
 use crate::core::ics24_host::identifier::ChainId;
+use crate::Height;
 
 use tendermint::trust_threshold::TrustThresholdFraction as TendermintTrustThresholdFraction;
 
 /// Provides an implementation of `ConnectionReader::validate_self_client` for
-/// Tendermint-based hosts. 
+/// Tendermint-based hosts.
 pub trait TendermintValidateSelfClientContext {
-    fn validate_self_client(
-        &self,
-        counterparty_client_state: Any,
-    ) -> Result<(), Error> {
+    fn validate_self_client(&self, counterparty_client_state: Any) -> Result<(), Error> {
         let counterparty_client_state = TmClientState::try_from(counterparty_client_state)
             .map_err(|_| {
                 Error::invalid_client_state("client must be a tendermint client".to_string())
