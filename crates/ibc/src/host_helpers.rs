70c76dc015f5af365a692372285f55cb59720445
Philippe Laferriere <plafer@protonmail.com>
2022-11-03 10:46:53 -0400

    remove `ConnectionReader` ctx from `validate_self_client`

--- crates/ibc/src/host_helpers.rs
+++ crates/ibc/src/host_helpers.rs
@@ -4,9 +4,9 @@ use alloc::format;
 use alloc::string::{String, ToString};
 use ibc_proto::google::protobuf::Any;
 
+use crate::Height;
 use crate::clients::ics07_tendermint::client_state::ClientState as TmClientState;
 use crate::core::ics02_client::client_state::ClientState;
-use crate::core::ics03_connection::context::ConnectionReader;
 use crate::core::ics03_connection::error::Error;
 use crate::core::ics23_commitment::specs::ProofSpecs;
 use crate::core::ics24_host::identifier::ChainId;
@@ -18,7 +18,6 @@ use tendermint::trust_threshold::TrustThresholdFraction as TendermintTrustThresh
 pub trait TendermintValidateSelfClientContext {
     fn validate_self_client(
         &self,
-        ctx: &dyn ConnectionReader,
         counterparty_client_state: Any,
     ) -> Result<(), Error> {
         let counterparty_client_state = TmClientState::try_from(counterparty_client_state)
@@ -47,11 +46,11 @@ pub trait TendermintValidateSelfClientContext {
             )));
         }
 
-        if counterparty_client_state.latest_height() >= ctx.host_current_height() {
+        if counterparty_client_state.latest_height() >= self.host_current_height() {
             return Err(Error::invalid_client_state(format!(
                 "client has latest height {} greater than or equal to chain height {}",
                 counterparty_client_state.latest_height(),
-                ctx.host_current_height()
+                self.host_current_height()
             )));
         }
 
@@ -102,6 +101,9 @@ pub trait TendermintValidateSelfClientContext {
 
     /// Returns the host chain id
     fn chain_id(&self) -> &ChainId;
+
+    /// Returns the host current height
+    fn host_current_height(&self) -> Height;
 
     /// Returns the host proof specs
     fn proof_specs(&self) -> &ProofSpecs;
