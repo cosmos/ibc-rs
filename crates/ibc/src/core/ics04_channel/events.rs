86b71ae5e02eac5e1afa17a593214da60fdb3014
Philippe Laferrière <plafer@protonmail.com>
2022-11-03 09:49:56 -0400

    Update crates/ibc/src/core/ics04_channel/events.rs
    Co-authored-by: Shoaib Ahmed <sufialhussaini@gmail.com>
    Signed-off-by: Philippe Laferrière <plafer@protonmail.com>

--- crates/ibc/src/core/ics04_channel/events.rs
+++ crates/ibc/src/core/ics04_channel/events.rs
@@ -655,7 +655,7 @@ impl TryFrom<AcknowledgePacket> for AbciEvent {
     type Error = Error;
 
     fn try_from(v: AcknowledgePacket) -> Result<Self, Self::Error> {
-        let mut attributes = Vec::with_capacity(11);
+        let mut attributes = Vec::with_capacity(9);
         attributes.push(v.timeout_height.into());
         attributes.push(v.timeout_timestamp.into());
         attributes.push(v.sequence.into());
