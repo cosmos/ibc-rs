96fda0aebb8763ba9244d8c542c10c8d19808ffe
Philippe Laferrière <plafer@protonmail.com>
2022-11-07 04:12:16 -0500

    Fix counterparty channel id unwrap (#220)
    * add failing test
    
    * bug fix
    
    * changelog

--- crates/ibc/src/core/ics04_channel/handler/verify.rs
+++ crates/ibc/src/core/ics04_channel/handler/verify.rs
@@ -38,7 +38,10 @@ pub fn verify_channel_proofs<Ctx: ChannelReader>(
             proofs.object_proof(),
             consensus_state.root(),
             channel_end.counterparty().port_id(),
-            channel_end.counterparty().channel_id().unwrap(),
+            channel_end
+                .counterparty()
+                .channel_id()
+                .ok_or_else(Error::invalid_counterparty_channel_id)?,
             expected_chan,
         )
         .map_err(Error::verify_channel_failed)
