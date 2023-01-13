ea7eb726484eaca5f52b400ad637b7eb01c062eb
Philippe Laferriere <plafer@protonmail.com>
2023-01-13 16:15:37 -0500

    chan_open_confirm_execute

--- crates/ibc/src/core/context.rs
+++ crates/ibc/src/core/context.rs
@@ -77,7 +77,7 @@ mod val_exec_ctx {
     use crate::core::ics04_channel::channel::{ChannelEnd, Counterparty, State};
     use crate::core::ics04_channel::commitment::{AcknowledgementCommitment, PacketCommitment};
     use crate::core::ics04_channel::context::calculate_block_delay;
-    use crate::core::ics04_channel::events::{OpenAck, OpenInit, OpenTry};
+    use crate::core::ics04_channel::events::{OpenAck, OpenConfirm, OpenInit, OpenTry};
     use crate::core::ics04_channel::handler::{
         chan_open_ack, chan_open_confirm, chan_open_init, chan_open_try,
     };
@@ -431,7 +431,9 @@ mod val_exec_ctx {
                         ChannelMsg::OpenAck(message) => {
                             chan_open_ack_execute(self, module_id, message)
                         }
-                        ChannelMsg::OpenConfirm(_) => todo!(),
+                        ChannelMsg::OpenConfirm(msg) => {
+                            chan_open_confirm_execute(self, module_id, msg)
+                        }
                         ChannelMsg::CloseInit(_) => todo!(),
                         ChannelMsg::CloseConfirm(_) => todo!(),
                     }
@@ -877,6 +879,73 @@ mod val_exec_ctx {
             .get_route(&module_id)
             .ok_or(ChannelError::RouteNotFound)?;
         module.on_chan_open_confirm_validate(&msg.port_id_on_b, &msg.chan_id_on_b)?;
+
+        Ok(())
+    }
+
+    fn chan_open_confirm_execute<ExecCtx>(
+        ctx_b: &mut ExecCtx,
+        module_id: ModuleId,
+        msg: MsgChannelOpenConfirm,
+    ) -> Result<(), ContextError>
+    where
+        ExecCtx: ExecutionContext,
+    {
+        let module = ctx_b
+            .get_route_mut(&module_id)
+            .ok_or(ChannelError::RouteNotFound)?;
+
+        let extras = module.on_chan_open_confirm_execute(&msg.port_id_on_b, &msg.chan_id_on_b)?;
+
+        let chan_end_on_b =
+            ctx_b.channel_end(&(msg.port_id_on_b.clone(), msg.chan_id_on_b.clone()))?;
+
+        // emit events and logs
+        {
+            ctx_b.log_message("success: channel open confirm".to_string());
+
+            let conn_id_on_b = chan_end_on_b.connection_hops[0].clone();
+            let port_id_on_a = chan_end_on_b.counterparty().port_id.clone();
+            let chan_id_on_a = chan_end_on_b
+                .counterparty()
+                .channel_id
+                .clone()
+                .ok_or(ContextError::ChannelError(ChannelError::Other {
+                    description:
+                        "internal error: ChannelEnd doesn't have a counterparty channel id in OpenConfirm"
+                            .to_string(),
+                }))?;
+
+            let core_event = IbcEvent::OpenConfirmChannel(OpenConfirm::new(
+                msg.port_id_on_b.clone(),
+                msg.chan_id_on_b.clone(),
+                port_id_on_a,
+                chan_id_on_a,
+                conn_id_on_b,
+            ));
+            ctx_b.emit_ibc_event(core_event);
+
+            for module_event in extras.events {
+                ctx_b.emit_ibc_event(IbcEvent::AppModule(module_event));
+            }
+
+            for log_message in extras.log {
+                ctx_b.log_message(log_message);
+            }
+        }
+
+        // state changes
+        {
+            let port_channel_id_on_b = (msg.port_id_on_b.clone(), msg.chan_id_on_b.clone());
+            let chan_end_on_b = {
+                let mut chan_end_on_b = chan_end_on_b;
+                chan_end_on_b.set_state(State::Open);
+
+                chan_end_on_b
+            };
+
+            ctx_b.store_channel(port_channel_id_on_b, chan_end_on_b)?;
+        }
 
         Ok(())
     }
