b0e88b790901f15566610ccf93c4fb12b1b84859
Philippe Laferriere <plafer@protonmail.com>
2023-01-23 16:42:29 -0500

    revert `Acknowledgement::AsRef` for `Error`

--- crates/ibc/src/applications/transfer/acknowledgement.rs
+++ crates/ibc/src/applications/transfer/acknowledgement.rs
@@ -45,10 +45,7 @@ impl AsRef<[u8]> for Acknowledgement {
     fn as_ref(&self) -> &[u8] {
         match self {
             Acknowledgement::Success(_) => r#"{"result":"AQ=="}"#.as_bytes(),
-            Acknowledgement::Error(_) => {
-                r#"{"error":"error handling packet on destination chain: see events for details"}"#
-                    .as_bytes()
-            }
+            Acknowledgement::Error(s) => s.as_bytes(),
         }
     }
 }
