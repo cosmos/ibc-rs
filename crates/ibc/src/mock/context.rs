cb69d28f2050c9a16ede96d12c302ec4a53927ff
Farhad Shabani <farhad.shabani@gmail.com>
2023-02-15 11:20:40 -0800

    Fix mock arg

--- crates/ibc/src/mock/context.rs
+++ crates/ibc/src/mock/context.rs
@@ -1159,7 +1159,10 @@ impl ConnectionReader for MockContext {
         Ok(self.ibc_store.lock().connection_ids_counter)
     }
 
-    fn validate_self_client(&self, _counterparty_client_state: Any) -> Result<(), ConnectionError> {
+    fn validate_self_client(
+        &self,
+        _host_client_state_on_counterparty: Any,
+    ) -> Result<(), ConnectionError> {
         Ok(())
     }
 }
