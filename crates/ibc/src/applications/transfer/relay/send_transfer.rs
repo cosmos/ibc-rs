1dcbce774c5a83aa4e5f5efbda93507b8a7f3152
Philippe Laferriere <plafer@protonmail.com>
2023-02-24 08:45:00 -0500

    send_transfer_validate: call send_packet_validate

--- crates/ibc/src/applications/transfer/relay/send_transfer.rs
+++ crates/ibc/src/applications/transfer/relay/send_transfer.rs
@@ -6,7 +6,7 @@ use crate::applications::transfer::events::TransferEvent;
 use crate::applications::transfer::msgs::transfer::MsgTransfer;
 use crate::applications::transfer::packet::PacketData;
 use crate::applications::transfer::{is_sender_chain_source, Coin, PrefixedCoin};
-use crate::core::ics04_channel::handler::send_packet::send_packet;
+use crate::core::ics04_channel::handler::send_packet::{send_packet, send_packet_validate};
 use crate::core::ics04_channel::packet::Packet;
 use crate::core::ics24_host::path::{ChannelEndPath, SeqSendPath};
 use crate::events::ModuleEvent;
@@ -139,28 +139,58 @@ where
         .channel_end(&chan_end_path_on_a)
         .map_err(TokenTransferError::ContextError)?;
 
-    let _chan_on_b = chan_end_on_a.counterparty().channel_id().ok_or_else(|| {
-        TokenTransferError::DestinationChannelNotFound {
+    let port_on_b = chan_end_on_a.counterparty().port_id().clone();
+    let chan_on_b = chan_end_on_a
+        .counterparty()
+        .channel_id()
+        .ok_or_else(|| TokenTransferError::DestinationChannelNotFound {
             port_id: msg.port_on_a.clone(),
             channel_id: msg.chan_on_a.clone(),
-        }
-    })?;
+        })?
+        .clone();
 
     let seq_send_path_on_a = SeqSendPath::new(&msg.port_on_a, &msg.chan_on_a);
-    let _sequence = ctx_a
+    let sequence = ctx_a
         .get_next_sequence_send(&seq_send_path_on_a)
         .map_err(TokenTransferError::ContextError)?;
 
-    let _token = msg
+    let token = msg
         .token
         .try_into()
         .map_err(|_| TokenTransferError::InvalidToken)?;
+    let denom = token.denom.clone();
+    let coin = Coin {
+        denom,
+        amount: token.amount,
+    };
 
     let _sender: Ctx::AccountId = msg
         .sender
         .clone()
         .try_into()
         .map_err(|_| TokenTransferError::ParseAccountFailure)?;
+
+    let data = {
+        let data = PacketData {
+            token: coin,
+            sender: msg.sender.clone(),
+            receiver: msg.receiver.clone(),
+        };
+        serde_json::to_vec(&data).expect("PacketData's infallible Serialize impl failed")
+    };
+
+    let packet = Packet {
+        sequence,
+        port_on_a: msg.port_on_a,
+        chan_on_a: msg.chan_on_a,
+        port_on_b,
+        chan_on_b,
+        data,
+        timeout_height_on_b: msg.timeout_height_on_b,
+        timeout_timestamp_on_b: msg.timeout_timestamp_on_b,
+    };
+
+    send_packet_validate(ctx_a, packet).map_err(TokenTransferError::ContextError)?;
 
     Ok(())
 }
