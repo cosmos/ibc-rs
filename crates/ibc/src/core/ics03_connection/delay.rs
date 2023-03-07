010f87ee7629d291dcbb350c5d24ed87cd5f468a
Farhad Shabani <farhad.shabani@gmail.com>
2023-03-07 11:20:29 -0800

    Use core::cmp::Ord for Timestamp comparison

--- crates/ibc/src/core/ics03_connection/delay.rs
+++ crates/ibc/src/core/ics03_connection/delay.rs
@@ -4,7 +4,7 @@ use super::{connection::ConnectionEnd, error::ConnectionError};
 
 pub fn verify_conn_delay_passed<Ctx>(
     ctx: &Ctx,
-    height: Height,
+    packet_proof_height: Height,
     connection_end: &ConnectionEnd,
 ) -> Result<(), ContextError>
 where
@@ -16,8 +16,8 @@ where
 
     // Fetch the latest time and height that the counterparty client was updated on the host chain.
     let client_id = connection_end.client_id();
-    let last_client_update_time = ctx.client_update_time(client_id, &height)?;
-    let last_client_update_height = ctx.client_update_height(client_id, &height)?;
+    let last_client_update_time = ctx.client_update_time(client_id, &packet_proof_height)?;
+    let last_client_update_height = ctx.client_update_height(client_id, &packet_proof_height)?;
 
     // Fetch the connection delay time and height periods.
     let conn_delay_time_period = connection_end.delay_period();
@@ -26,8 +26,7 @@ where
     // Verify that the current host chain time is later than the last client update time
     let earliest_valid_time = (last_client_update_time + conn_delay_time_period)
         .map_err(ConnectionError::TimestampOverflow)?;
-    if !(current_host_time == earliest_valid_time || current_host_time.after(&earliest_valid_time))
-    {
+    if current_host_time < earliest_valid_time {
         return Err(ConnectionError::NotEnoughTimeElapsed {
             current_host_time,
             earliest_valid_time,
