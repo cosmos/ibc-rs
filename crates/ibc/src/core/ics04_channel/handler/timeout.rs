e516af1557dbed689b4f1f84fd1c27e096a6562f
yito88 <yuji@phact-columba.com>
2023-03-20 17:30:30 +0100

    fix error checking in a unit test

--- crates/ibc/src/core/ics04_channel/handler/timeout.rs
+++ crates/ibc/src/core/ics04_channel/handler/timeout.rs
@@ -347,10 +347,8 @@ mod tests {
         let res = validate(&context, &msg);
 
         assert!(
-            res.unwrap_err()
-                .to_string()
-                .starts_with("ICS04 Packet error: Packet timeout"),
-            "Validation should fail because both the timeout height and timestamp have not reached yet"
+            res.is_err(),
+            "Validation should fail because the timeout height was reached, but the timestamp hasn't been reached. Both the height and timestamp need to be reached for the packet to be considered timed out"
         )
     }
 
