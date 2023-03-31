24936656e42cf1afb39ce8e5384c6b41c8e427de
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2023-03-31 14:16:07 -0700

    tx_msg: Remove panic in `Msg::get_sign_bytes`

---
+++ .changelog/unreleased/bug/593-remove-tx-msg-panic.md
@@ -0,0 +1,2 @@
+- tx_msg: Remove panic in `Msg::get_sign_bytes`
+  ([#593](https://github.com/cosmos/ibc-rs/issues/593))
--- crates/ibc/src/tx_msg.rs
+++ crates/ibc/src/tx_msg.rs
@@ -8,16 +8,8 @@ pub trait Msg: Clone {
     fn type_url(&self) -> String;
 
     fn get_sign_bytes(self) -> Vec<u8> {
-        let mut buf = Vec::new();
         let raw_msg: Self::Raw = self.into();
-        match prost::Message::encode(&raw_msg, &mut buf) {
-            Ok(()) => buf,
-            // Severe error that cannot be recovered.
-            Err(e) => panic!(
-                "Cannot encode the proto message {:?} into a buffer due to underlying error: {}",
-                raw_msg, e
-            ),
-        }
+        prost::Message::encode_to_vec(&raw_msg)
     }
 
     fn to_any(self) -> Any {
