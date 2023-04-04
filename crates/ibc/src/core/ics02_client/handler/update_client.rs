0ba0c05b21d7a43028e0daed0f349a8707f5c21e
Philippe Laferriere <plafer@protonmail.com>
2023-04-04 09:04:42 -0400

    move misbehaviour tests to update_client

--- crates/ibc/src/core/ics02_client/handler/update_client.rs
+++ crates/ibc/src/core/ics02_client/handler/update_client.rs
@@ -135,7 +135,10 @@ mod tests {
 
     use crate::clients::ics07_tendermint::client_type as tm_client_type;
     use crate::clients::ics07_tendermint::consensus_state::ConsensusState as TmConsensusState;
+    use crate::clients::ics07_tendermint::header::Header as TmHeader;
+    use crate::clients::ics07_tendermint::misbehaviour::Misbehaviour as TmMisbehaviour;
     use crate::core::ics02_client::client_state::ClientState;
+    use crate::core::ics02_client::client_type::ClientType;
     use crate::core::ics02_client::consensus_state::downcast_consensus_state;
     use crate::core::ics02_client::handler::update_client::{execute, validate};
     use crate::core::ics02_client::msgs::update_client::{MsgUpdateClient, UpdateClientKind};
@@ -146,6 +149,7 @@ mod tests {
     use crate::mock::client_state::MockClientState;
     use crate::mock::context::MockContext;
     use crate::mock::header::MockHeader;
+    use crate::mock::misbehaviour::Misbehaviour as MockMisbehaviour;
     use crate::mock::host::{HostBlock, HostType};
     use crate::test_utils::get_dummy_account_id;
     use crate::timestamp::Timestamp;
@@ -433,5 +437,204 @@ mod tests {
         assert_eq!(update_client_event.consensus_height(), &height);
         assert_eq!(update_client_event.consensus_heights(), &vec![height]);
         assert_eq!(update_client_event.header(), &header);
+    }
+
+    fn ensure_misbehaviour(ctx: &MockContext, client_id: &ClientId, client_type: &ClientType) {
+        let client_state = ctx.client_state(client_id).unwrap();
+
+        assert!(client_state.confirm_not_frozen().is_err());
+
+        // check events
+        assert_eq!(ctx.events.len(), 2);
+        assert!(matches!(
+            ctx.events[0],
+            IbcEvent::Message(IbcEventType::ClientMisbehaviour),
+        ));
+        let misbehaviour_client_event =
+            downcast!(&ctx.events[1] => IbcEvent::ClientMisbehaviour).unwrap();
+        assert_eq!(misbehaviour_client_event.client_id(), client_id);
+        assert_eq!(misbehaviour_client_event.client_type(), client_type);
+    }
+
+    /// Tests misbehaviour handling for the mock client.
+    /// Misbehaviour evidence consists of identical headers - mock misbehaviour handler considers it
+    /// a valid proof of misbehaviour
+    #[test]
+    fn test_misbehaviour_client_ok() {
+        let client_id = ClientId::default();
+        let timestamp = Timestamp::now();
+        let height = Height::new(0, 46).unwrap();
+        let msg = MsgUpdateClient {
+            client_id: client_id.clone(),
+            client_message: MockMisbehaviour {
+                client_id: client_id.clone(),
+                header1: MockHeader::new(height).with_timestamp(timestamp),
+                header2: MockHeader::new(height).with_timestamp(timestamp),
+            }
+            .into(),
+            update_kind: UpdateClientKind::UpdateHeader,
+            signer: get_dummy_account_id(),
+        };
+
+        let mut ctx = MockContext::default().with_client(&client_id, Height::new(0, 42).unwrap());
+
+        let res = validate(&ctx, msg.clone());
+        assert!(res.is_ok());
+        let res = execute(&mut ctx, msg);
+        assert!(res.is_ok());
+
+        ensure_misbehaviour(&ctx, &client_id, &mock_client_type());
+    }
+
+    /// Tests misbehaviour handling failure for a non-existent client
+    #[test]
+    fn test_misbehaviour_nonexisting_client() {
+        let client_id = ClientId::from_str("mockclient1").unwrap();
+        let height = Height::new(0, 46).unwrap();
+        let msg = MsgUpdateClient {
+            client_id: ClientId::from_str("nonexistingclient").unwrap(),
+            client_message: MockMisbehaviour {
+                client_id: client_id.clone(),
+                header1: MockHeader::new(height),
+                header2: MockHeader::new(height),
+            }
+            .into(),
+            update_kind: UpdateClientKind::Misbehaviour,
+            signer: get_dummy_account_id(),
+        };
+
+        let ctx = MockContext::default().with_client(&client_id, Height::new(0, 42).unwrap());
+        let res = validate(&ctx, msg);
+        assert!(res.is_err());
+    }
+
+    /// Tests misbehaviour handling for the synthetic Tendermint client.
+    /// Misbehaviour evidence consists of equivocal headers.
+    #[test]
+    fn test_misbehaviour_synthetic_tendermint_equivocation() {
+        let client_id = ClientId::new(tm_client_type(), 0).unwrap();
+        let client_height = Height::new(1, 20).unwrap();
+        let misbehaviour_height = Height::new(1, 21).unwrap();
+        let chain_id_b = ChainId::new("mockgaiaB".to_string(), 1);
+
+        // Create a mock context for chain-A with a synthetic tendermint light client for chain-B
+        let mut ctx_a = MockContext::new(
+            ChainId::new("mockgaiaA".to_string(), 1),
+            HostType::Mock,
+            5,
+            Height::new(1, 1).unwrap(),
+        )
+        .with_client_parametrized_with_chain_id(
+            chain_id_b.clone(),
+            &client_id,
+            client_height,
+            Some(tm_client_type()),
+            Some(client_height),
+        );
+
+        // Create a mock context for chain-B
+        let ctx_b = MockContext::new(
+            chain_id_b.clone(),
+            HostType::SyntheticTendermint,
+            5,
+            misbehaviour_height,
+        );
+
+        // Get chain-B's header at `misbehaviour_height`
+        let header1: TmHeader = {
+            let mut block = ctx_b.host_block(&misbehaviour_height).unwrap().clone();
+            block.set_trusted_height(client_height);
+            block.try_into_tm_block().unwrap().into()
+        };
+
+        // Generate an equivocal header for chain-B at `misbehaviour_height`
+        let header2 = {
+            let mut tm_block = HostBlock::generate_tm_block(
+                chain_id_b,
+                misbehaviour_height.revision_height(),
+                Timestamp::now(),
+            );
+            tm_block.trusted_height = client_height;
+            tm_block.into()
+        };
+
+        let msg = MsgUpdateClient {
+            client_id: client_id.clone(),
+            client_message: TmMisbehaviour::new(client_id.clone(), header1, header2)
+                .unwrap()
+                .into(),
+            update_kind: UpdateClientKind::Misbehaviour,
+            signer: get_dummy_account_id(),
+        };
+
+        let res = validate(&ctx_a, msg.clone());
+        assert!(res.is_ok());
+        let res = execute(&mut ctx_a, msg);
+        assert!(res.is_ok());
+        ensure_misbehaviour(&ctx_a, &client_id, &tm_client_type());
+    }
+
+    #[test]
+    fn test_misbehaviour_synthetic_tendermint_bft_time() {
+        let client_id = ClientId::new(tm_client_type(), 0).unwrap();
+        let client_height = Height::new(1, 20).unwrap();
+        let misbehaviour_height = Height::new(1, 21).unwrap();
+        let chain_id_b = ChainId::new("mockgaiaB".to_string(), 1);
+
+        // Create a mock context for chain-A with a synthetic tendermint light client for chain-B
+        let mut ctx_a = MockContext::new(
+            ChainId::new("mockgaiaA".to_string(), 1),
+            HostType::Mock,
+            5,
+            Height::new(1, 1).unwrap(),
+        )
+        .with_client_parametrized_with_chain_id(
+            chain_id_b.clone(),
+            &client_id,
+            client_height,
+            Some(tm_client_type()),
+            Some(client_height),
+        );
+
+        // Generate `header1` for chain-B
+        let header1 = {
+            let mut tm_block = HostBlock::generate_tm_block(
+                chain_id_b.clone(),
+                misbehaviour_height.revision_height(),
+                Timestamp::now(),
+            );
+            tm_block.trusted_height = client_height;
+            tm_block
+        };
+
+        // Generate `header2` for chain-B which is identical to `header1` but with a conflicting
+        // timestamp
+        let header2 = {
+            let timestamp =
+                Timestamp::from_nanoseconds(Timestamp::now().nanoseconds() + 1_000_000_000)
+                    .unwrap();
+            let mut tm_block = HostBlock::generate_tm_block(
+                chain_id_b,
+                misbehaviour_height.revision_height(),
+                timestamp,
+            );
+            tm_block.trusted_height = client_height;
+            tm_block
+        };
+
+        let msg = MsgUpdateClient {
+            client_id: client_id.clone(),
+            client_message: TmMisbehaviour::new(client_id.clone(), header1.into(), header2.into())
+                .unwrap()
+                .into(),
+            update_kind: UpdateClientKind::Misbehaviour,
+            signer: get_dummy_account_id(),
+        };
+
+        let res = validate(&ctx_a, msg.clone());
+        assert!(res.is_ok());
+        let res = execute(&mut ctx_a, msg);
+        assert!(res.is_ok());
+        ensure_misbehaviour(&ctx_a, &client_id, &tm_client_type());
     }
 }
