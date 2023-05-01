afa0f76f7edc3101055e9cfd7f6ee5a848b09e7e
Philippe Laferriere <plafer@protonmail.com>
2023-05-01 14:36:25 -0400

    some docstrings updated

--- crates/ibc/src/clients/mod.rs
+++ crates/ibc/src/clients/mod.rs
@@ -1,3 +1,5 @@
-//! Implementations of client verification algorithms for specific types of chains.
+//! Light client implementations to be used in [Core](core).
+//! 
+//! [core]: https://github.com/cosmos/ibc-rs/tree/main/crates/ibc/src/core
 
 pub mod ics07_tendermint;
