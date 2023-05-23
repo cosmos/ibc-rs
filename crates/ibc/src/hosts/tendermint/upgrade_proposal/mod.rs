2950ab7232f7d0514204334fa772faf6c4a43da4
Farhad Shabani <farhad.shabani@gmail.com>
2023-05-23 09:58:21 -0700

    Re-add upgrade_client_proposal_handler

--- crates/ibc/src/hosts/tendermint/upgrade_proposal/mod.rs
+++ crates/ibc/src/hosts/tendermint/upgrade_proposal/mod.rs
@@ -1,9 +1,11 @@
 mod context;
 mod events;
+mod handler;
 mod plan;
 mod proposal;
 
 pub use context::{UpgradeExecutionContext, UpgradeValidationContext};
 pub use events::{UpgradeChain, UpgradeClientProposal};
+pub use handler::upgrade_client_proposal_handler;
 pub use plan::Plan;
 pub use proposal::UpgradeProposal;
