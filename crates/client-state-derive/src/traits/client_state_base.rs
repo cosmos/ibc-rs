a9693dfef6719bbdc951c95e849ed58864fc5520
Philippe Laferriere <plafer@protonmail.com>
2023-05-31 16:28:35 -0400

    latest_height

--- crates/client-state-derive/src/traits/client_state_base.rs
+++ crates/client-state-derive/src/traits/client_state_base.rs
@@ -13,6 +13,7 @@ pub fn impl_ClientStateBase(
     enum_variants: &Punctuated<Variant, Comma>,
 ) -> TokenStream {
     let client_type_impl = client_type(enum_name, enum_variants.iter());
+    let latest_height_impl = latest_height(enum_name, enum_variants.iter());
     let validate_proof_height_impl = validate_proof_height(&enum_name, enum_variants.iter());
 
     let ClientStateBase = Imports::ClientStateBase();
@@ -25,6 +26,11 @@ pub fn impl_ClientStateBase(
             fn client_type(&self) -> #ClientType {
                 match self {
                     #(#client_type_impl),*
+                }
+            }
+            fn latest_height(&self) -> #Height {
+                match self {
+                    #(#latest_height_impl),*
                 }
             }
             fn validate_proof_height(&self, proof_height: #Height) -> Result<(), #ClientError> {
@@ -46,6 +52,20 @@ fn client_type(enum_name: &Ident, enum_variants: Iter<Variant>) -> Vec<TokenStre
             let variant_type_name = get_enum_variant_type_path(variant);
             quote! {
                 #enum_name::#variant_name(cs) => <#variant_type_name as #ClientStateBase>::client_type(cs)
+            }
+        })
+        .collect()
+}
+
+fn latest_height(enum_name: &Ident, enum_variants: Iter<Variant>) -> Vec<TokenStream> {
+    let ClientStateBase = Imports::ClientStateBase();
+
+    enum_variants
+        .map(|variant| {
+            let variant_name = &variant.ident;
+            let variant_type_name = get_enum_variant_type_path(variant);
+            quote! {
+                #enum_name::#variant_name(cs) => <#variant_type_name as #ClientStateBase>::latest_height(cs)
             }
         })
         .collect()
