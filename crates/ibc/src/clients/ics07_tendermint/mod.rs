934ad76e868bc9f90b13c2ad598e08c0a9b121c8
Philippe Laferriere <plafer@protonmail.com>
2023-06-26 14:28:12 -0400

    add ClientType test

--- crates/ibc/src/clients/ics07_tendermint/mod.rs
+++ crates/ibc/src/clients/ics07_tendermint/mod.rs
@@ -20,3 +20,14 @@ pub(crate) const TENDERMINT_CLIENT_TYPE: &str = "07-tendermint";
 pub fn client_type() -> ClientType {
     ClientType::from_str(TENDERMINT_CLIENT_TYPE).expect("Never fails because it's valid")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Ensures that the validation in `ClientType::from_str` doesn't fail for the tendermint client type
+    #[test]
+    pub fn test_tm_client_type() {
+        let _ = ClientType::from_str(TENDERMINT_CLIENT_TYPE).unwrap();
+    }
+}
