9ebe9e570cd1f4bca08f1e29a4452f7a429b1495
Ranadeep Biswas <mail@rnbguy.at>
2023-09-14 16:53:05 +0200

    rm reexports

--- crates/ibc/src/services/core/mod.rs
+++ crates/ibc/src/services/core/mod.rs
@@ -2,7 +2,3 @@ pub mod channel;
 pub mod client;
 pub mod connection;
 pub mod context;
-
-pub use channel::ChannelQueryService;
-pub use client::ClientQueryService;
-pub use connection::ConnectionQueryService;
