701ecb3413f66b361c6b9634e3be8d7a4e294055
Farhad Shabani <farhad.shabani@gmail.com>
2023-10-12 22:15:52 -0700

    test: calculate_block_delay

--- crates/ibc/src/core/ics04_channel/context.rs
+++ crates/ibc/src/core/ics04_channel/context.rs
@@ -148,5 +148,35 @@ pub(crate) fn calculate_block_delay(
     if delay_period_time % max_expected_time_per_block == 0 {
         return delay_period_time / max_expected_time_per_block;
     }
-    u64::max(1, delay_period_time / max_expected_time_per_block)
+    u64::max(1, (delay_period_time / max_expected_time_per_block) + 1)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use rstest::rstest;
+
+    use crate::core::ics04_channel::context::calculate_block_delay;
+
+    #[rstest]
+    #[case::remainder_zero(10, 2, 5)]
+    #[case::remainder_not_zero(10, 3, 4)]
+    #[case::max_expected_zero(10, 0, 0)]
+    #[case::delay_period_zero(0, 2, 0)]
+    #[case::both_zero(0, 0, 0)]
+    #[case::delay_less_than_max(10, 11, 1)]
+    fn test_calculate_block_delay_zero(
+        #[case] delay_period_time: u64,
+        #[case] max_expected_time_per_block: u64,
+        #[case] expected: u64,
+    ) {
+        assert_eq!(
+            calculate_block_delay(
+                &Duration::from_secs(delay_period_time),
+                &Duration::from_secs(max_expected_time_per_block)
+            ),
+            expected
+        );
+    }
 }
