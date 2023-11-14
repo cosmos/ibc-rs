81baf299eafd834d4739b1972dddea61e73e80ed
Ranadeep Biswas <mail@rnbguy.at>
2023-11-14 09:01:48 -0500

    use enum tuple variants directly

--- crates/ibc/src/core/ics03_connection/delay.rs
+++ crates/ibc/src/core/ics03_connection/delay.rs
@@ -33,21 +33,19 @@ where
     let earliest_valid_time = (last_client_update_time + conn_delay_time_period)
         .map_err(ConnectionError::TimestampOverflow)?;
     if current_host_time < earliest_valid_time {
-        return Err(ConnectionError::NotEnoughTimeElapsed {
+        return Err(ContextError::ConnectionError(ConnectionError::NotEnoughTimeElapsed {
             current_host_time,
             earliest_valid_time,
-        })
-        .map_err(ContextError::ConnectionError);
+        }));
     }
 
     // Verify that the current host chain height is later than the last client update height
     let earliest_valid_height = last_client_update_height.add(conn_delay_height_period);
     if current_host_height < earliest_valid_height {
-        return Err(ConnectionError::NotEnoughBlocksElapsed {
+        return Err(ContextError::ConnectionError(ConnectionError::NotEnoughBlocksElapsed {
             current_host_height,
             earliest_valid_height,
-        })
-        .map_err(ContextError::ConnectionError);
+        }));
     };
 
     Ok(())
