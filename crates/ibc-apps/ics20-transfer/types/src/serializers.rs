ff32e98789c635cc19b341966e2bc2a68ce525fc
Farhad Shabani <farhad.shabani@gmail.com>
2023-11-17 07:23:22 -0800

    fix: cargo fmt

--- crates/ibc-apps/ics20-transfer/types/src/serializers.rs
+++ crates/ibc-apps/ics20-transfer/types/src/serializers.rs
@@ -7,7 +7,7 @@ use serde::{de, Deserialize, Deserializer, Serializer};
 // Note: This method serializes to a String instead of a str
 // in order to avoid a wasm compilation issue. Specifically,
 // str (de)serialization hits some kind of f64/f32 case
-// when compiled into wasm, but this fails validation on 
+// when compiled into wasm, but this fails validation on
 // f32/f64 wasm runtimes.
 pub fn serialize<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
 where
