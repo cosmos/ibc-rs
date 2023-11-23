e2a0efedfd16d5b1aeeec644cf190861332fd78b
Farhad Shabani <farhad.shabani@gmail.com>
2023-11-23 09:51:57 -0800

    fix: apply reviewer comments

--- ibc-core/ics25-handler/src/lib.rs
+++ ibc-core/ics25-handler/src/lib.rs
@@ -5,11 +5,10 @@
 //!
 //! When processing a given message `M`, if any method in this library returns
 //! an error, the runtime is expected to rollback all state modifications made
-//! to the context (e.g.
-//! [`ExecutionContext`](ibc_core_host::ExecutionContext)) while processing
-//! `M`. If a transaction on your blockchain contains multiple messages, then
-//! typically the state modifications from all messages is expected to be rolled
-//! back as well.
+//! to the context (e.g. [`ExecutionContext`](ibc_core_host::ExecutionContext))
+//! while processing `M`. If the transaction containing `M` consists of multiple
+//! messages, then typically the state modifications from all messages is
+//! expected to be rolled back as well.
 #![no_std]
 #![forbid(unsafe_code)]
 #![cfg_attr(not(test), deny(clippy::unwrap_used))]
