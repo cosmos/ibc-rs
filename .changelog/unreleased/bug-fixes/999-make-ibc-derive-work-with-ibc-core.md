fcc8c576f2998ce9108c228b4e9f5680d2bef932
Farhad Shabani <Farhad.Shabani@gmail.com>
2024-01-11 18:17:49 -0800

    fix: refactor `ibc-derive` to handle context with generic types and projects dependent on `ibc-core` (#1037)
    * fix: refactor ibc-derive to support broader cases
    
    * fix: cargo doc
    
    * imp: define get_impl_quote
    
    * imp: move each set of derives to the related meta-crate
    
    * imp: refactor ClientState drive to work with generics with trait-bounds
    
    * fix: remove redundant derive feature
    
    * docs: improve derive docstrings
    
    * imp: define SupportedCrate enum

---
+++ .changelog/unreleased/bug-fixes/999-make-ibc-derive-work-with-ibc-core.md
@@ -0,0 +1,2 @@
+- [ibc-derive] Adapt macro derivations to integrate with projects dependent on
+  `ibc-core` ([\#999](https://github.com/cosmos/ibc-rs/issues/999)).
--- ibc-derive/src/lib.rs
+++ ibc-derive/src/lib.rs
@@ -16,21 +16,47 @@ use client_state::client_state_derive_impl;
 use consensus_state::consensus_state_derive_impl;
 use proc_macro::TokenStream as RawTokenStream;
 use syn::{parse_macro_input, DeriveInput};
+use utils::{Imports, SupportedCrate};
 
-#[proc_macro_derive(ClientState, attributes(generics, mock))]
-pub fn client_state_macro_derive(input: RawTokenStream) -> RawTokenStream {
+#[proc_macro_derive(IbcClientState, attributes(validation, execution))]
+pub fn ibc_client_state_macro_derive(input: RawTokenStream) -> RawTokenStream {
+    generate_client_state_derive(input, SupportedCrate::Ibc)
+}
+
+#[proc_macro_derive(IbcCoreClientState, attributes(validation, execution))]
+pub fn ibc_core_client_state_macro_derive(input: RawTokenStream) -> RawTokenStream {
+    generate_client_state_derive(input, SupportedCrate::IbcCore)
+}
+
+fn generate_client_state_derive(input: RawTokenStream, source: SupportedCrate) -> RawTokenStream {
     let ast: DeriveInput = parse_macro_input!(input);
 
-    let output = client_state_derive_impl(ast);
+    let imports = Imports::new(source);
+
+    let output = client_state_derive_impl(ast, &imports);
 
     RawTokenStream::from(output)
 }
 
-#[proc_macro_derive(ConsensusState)]
-pub fn consensus_state_macro_derive(input: RawTokenStream) -> RawTokenStream {
+#[proc_macro_derive(IbcConsensusState)]
+pub fn ibc_consensus_state_macro_derive(input: RawTokenStream) -> RawTokenStream {
+    generate_consensus_state_derive(input, SupportedCrate::Ibc)
+}
+
+#[proc_macro_derive(IbcCoreConsensusState)]
+pub fn ibc_core_consensus_state_macro_derive(input: RawTokenStream) -> RawTokenStream {
+    generate_consensus_state_derive(input, SupportedCrate::IbcCore)
+}
+
+fn generate_consensus_state_derive(
+    input: RawTokenStream,
+    source: SupportedCrate,
+) -> RawTokenStream {
     let ast: DeriveInput = parse_macro_input!(input);
 
-    let output = consensus_state_derive_impl(ast);
+    let imports = Imports::new(source);
+
+    let output = consensus_state_derive_impl(ast, &imports);
 
     RawTokenStream::from(output)
 }
