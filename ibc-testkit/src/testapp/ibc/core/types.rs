304dd6330e26a8faf5e138282116b4b8f39d657b
Ranadeep Biswas <mail@rnbguy.at>
2024-01-23 23:52:58 +0100

    fix blocks_since

--- ibc-testkit/src/testapp/ibc/core/types.rs
+++ ibc-testkit/src/testapp/ibc/core/types.rs
@@ -451,8 +451,8 @@ impl MockContext {
         };
 
         fn blocks_since(a: Height, b: Height) -> Option<u64> {
-            (a.revision_number() != b.revision_number()
-                || a.revision_height() < b.revision_height())
+            (a.revision_number() == b.revision_number()
+                && a.revision_height() >= b.revision_height())
             .then(|| a.revision_height() - b.revision_height())
         }
 
