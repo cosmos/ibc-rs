5c0febdc7e316019701e82a377afc5fc230e8739
Farhad Shabani <farhad.shabani@gmail.com>
2024-01-24 06:51:57 -0800

    fix: re-export a few necessary types

--- ibc-primitives/src/lib.rs
+++ ibc-primitives/src/lib.rs
@@ -25,9 +25,9 @@ pub use traits::*;
 mod types;
 pub use types::*;
 
-/// Re-exports google proto types and `Protobuf` trait from the `ibc-proto-rs`
-/// crate.
+/// Re-exports necessary google proto types and `Protobuf` trait from the
+/// `ibc-proto-rs` crate.
 pub mod proto {
-    pub use ibc_proto::google::protobuf::*;
+    pub use ibc_proto::google::protobuf::{Any, Duration, Timestamp};
     pub use ibc_proto::Protobuf;
 }
