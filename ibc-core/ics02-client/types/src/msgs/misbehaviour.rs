06288a648dad6490c48eb27d35050f8da66dc160
Sean Chen <seanchen11235@gmail.com>
2024-02-06 14:07:01 -0600

    Decorate MsgSubmitMisbehaviour with `deprecated` attribute

--- ibc-core/ics02-client/types/src/msgs/misbehaviour.rs
+++ ibc-core/ics02-client/types/src/msgs/misbehaviour.rs
@@ -12,6 +12,13 @@ use crate::error::ClientError;
 pub const SUBMIT_MISBEHAVIOUR_TYPE_URL: &str = "/ibc.core.client.v1.MsgSubmitMisbehaviour";
 
 /// A type of message that submits client misbehaviour proof.
+///
+/// Deprecated since v0.50.0. Misbehaviour reports should be submitted via the `MsgUpdateClient`
+/// type through its `client_message` field.
+#[deprecated(
+    since = "0.50.0",
+    note = "Misbehaviour reports should be submitted via `MsgUpdateClient` through its `client_message` field"
+)]
 #[cfg_attr(
     feature = "borsh",
     derive(borsh::BorshSerialize, borsh::BorshDeserialize)
