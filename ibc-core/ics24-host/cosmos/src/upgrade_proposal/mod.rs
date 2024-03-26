8c76ff9e95143774a55f5a51a93c6120d9888335
Sean Chen <seanchen11235@gmail.com>
2024-03-26 11:07:13 -0500

    Add upgrade proposal type url

--- ibc-core/ics24-host/cosmos/src/upgrade_proposal/mod.rs
+++ ibc-core/ics24-host/cosmos/src/upgrade_proposal/mod.rs
@@ -10,4 +10,4 @@ pub use context::*;
 pub use events::{UpgradeChain, UpgradeClientProposal};
 pub use handler::execute_upgrade_client_proposal;
 pub use plan::Plan;
-pub use proposal::UpgradeProposal;
+pub use proposal::*;
