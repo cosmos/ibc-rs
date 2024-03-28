e555c7f7a178502c05ef48ee33b2a3ed09043d9b
Sean Chen <seanchen11235@gmail.com>
2024-03-28 16:21:46 -0500

    Stub out `test_recover_client_ok`

--- ibc-testkit/tests/core/ics02_client/recover_client.rs
+++ ibc-testkit/tests/core/ics02_client/recover_client.rs
@@ -1,7 +1,9 @@
 use ibc::core::client::types::msgs::ClientMsg;
 use ibc::core::client::types::{msgs::MsgRecoverClient, Height};
+use ibc::core::entrypoint::{execute, validate};
 use ibc::core::handler::types::msgs::MsgEnvelope;
 use ibc::core::host::types::identifiers::ClientId;
+use ibc_testkit::testapp::ibc::core::router::MockRouter;
 use ibc_testkit::{
     fixtures::core::signer::dummy_account_id,
     testapp::ibc::core::types::{MockClientConfig, MockContext},
@@ -11,6 +13,7 @@ use rstest::*;
 
 struct Fixture {
     ctx: MockContext,
+    router: MockRouter,
 }
 
 #[fixture]
@@ -24,12 +27,17 @@ fn fixture() -> Fixture {
             .build(),
     );
 
-    Fixture { ctx }
+    let router = MockRouter::new_with_transfer();
+
+    Fixture { ctx, router }
 }
 
 #[rstest]
 fn test_recover_client_ok(fixture: Fixture) {
-    let Fixture { ctx } = fixture;
+    let Fixture {
+        mut ctx,
+        mut router,
+    } = fixture;
 
     let subject_client_id = ClientId::new("07-tendermint", 0).expect("no error");
     let substitute_client_id = ClientId::new("07-tendermint", 1).expect("no error");
@@ -42,4 +50,14 @@ fn test_recover_client_ok(fixture: Fixture) {
     };
 
     let msg_envelope = MsgEnvelope::from(ClientMsg::from(msg.clone()));
+
+    let res = validate(&ctx, &router, msg_envelope.clone());
+
+    assert!(res.is_ok(), "validation happy path");
+
+    let res = execute(&mut ctx, &mut router, msg_envelope);
+
+    assert!(res.is_ok(), "execution happy path");
+
+    // assert that the subject's client state is as expected
 }
