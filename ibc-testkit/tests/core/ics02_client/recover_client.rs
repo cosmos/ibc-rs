b05a5a1cb686b675366a277d6f3901ada33b51e9
Sean Chen <seanchen11235@gmail.com>
2024-04-01 14:54:39 -0500

    Update recover client test fixture

--- ibc-testkit/tests/core/ics02_client/recover_client.rs
+++ ibc-testkit/tests/core/ics02_client/recover_client.rs
@@ -1,35 +1,75 @@
-use ibc::core::client::types::msgs::ClientMsg;
+use ibc::core::client::context::ClientValidationContext;
+use ibc::core::client::types::msgs::{ClientMsg, MsgCreateClient};
 use ibc::core::client::types::{msgs::MsgRecoverClient, Height};
 use ibc::core::entrypoint::{execute, validate};
 use ibc::core::handler::types::msgs::MsgEnvelope;
 use ibc::core::host::types::identifiers::ClientId;
-use ibc_testkit::testapp::ibc::core::router::MockRouter;
-use ibc_testkit::{
-    fixtures::core::signer::dummy_account_id,
-    testapp::ibc::core::types::{MockClientConfig, MockContext},
+use ibc::core::host::ValidationContext;
+use ibc::core::primitives::Signer;
+use ibc_testkit::fixtures::core::signer::dummy_account_id;
+use ibc_testkit::testapp::ibc::clients::mock::client_state::{
+    client_type as mock_client_type, MockClientState,
 };
+use ibc_testkit::testapp::ibc::clients::mock::consensus_state::MockConsensusState;
+use ibc_testkit::testapp::ibc::clients::mock::header::MockHeader;
+use ibc_testkit::testapp::ibc::core::router::MockRouter;
+use ibc_testkit::testapp::ibc::core::types::MockContext;
 
 use rstest::*;
 
 struct Fixture {
     ctx: MockContext,
     router: MockRouter,
+    subject_client_id: ClientId,
+    substitute_client_id: ClientId,
+    signer: Signer,
 }
 
 #[fixture]
 fn fixture() -> Fixture {
-    let client_id = ClientId::new("07-tendermint", 0).expect("no error");
+    let mut ctx = MockContext::default();
+    let mut router = MockRouter::new_with_transfer();
+    let signer = dummy_account_id();
+    let height = Height::new(0, 42).unwrap();
 
-    let ctx = MockContext::default().with_client_config(
-        MockClientConfig::builder()
-            .client_id(client_id.clone())
-            .latest_height(Height::new(0, 42).unwrap())
-            .build(),
+    // Create the subject client
+    let msg = MsgCreateClient::new(
+        MockClientState::new(MockHeader::new(height)).into(),
+        MockConsensusState::new(MockHeader::new(height)).into(),
+        signer.clone(),
     );
 
-    let router = MockRouter::new_with_transfer();
+    let msg_envelope = MsgEnvelope::from(ClientMsg::from(msg));
 
-    Fixture { ctx, router }
+    let client_type = mock_client_type();
+    let subject_client_id = client_type.build_client_id(ctx.client_counter().unwrap());
+
+    validate(&ctx, &router, msg_envelope.clone()).expect("create client validation");
+    execute(&mut ctx, &mut router, msg_envelope).expect("create client execution");
+
+    // Create the substitute client
+    let height = height.increment();
+
+    let msg = MsgCreateClient::new(
+        MockClientState::new(MockHeader::new(height)).into(),
+        MockConsensusState::new(MockHeader::new(height)).into(),
+        signer.clone(),
+    );
+
+    let msg_envelope = MsgEnvelope::from(ClientMsg::from(msg));
+
+    let substitute_client_id = client_type.build_client_id(ctx.client_counter().unwrap());
+
+    validate(&ctx, &router, msg_envelope.clone()).expect("create client validation");
+    execute(&mut ctx, &mut router, msg_envelope).expect("create client execution");
+
+    Fixture {
+        ctx,
+        router,
+        subject_client_id,
+        substitute_client_id,
+        signer,
+    }
 }
 
 #[rstest]
@@ -37,11 +77,10 @@ fn test_recover_client_ok(fixture: Fixture) {
     let Fixture {
         mut ctx,
         mut router,
+        subject_client_id,
+        substitute_client_id,
+        signer,
     } = fixture;
-
-    let subject_client_id = ClientId::new("07-tendermint", 0).expect("no error");
-    let substitute_client_id = ClientId::new("07-tendermint", 1).expect("no error");
-    let signer = dummy_account_id();
 
     let msg = MsgRecoverClient {
         subject_client_id,
@@ -53,11 +92,22 @@ fn test_recover_client_ok(fixture: Fixture) {
 
     let res = validate(&ctx, &router, msg_envelope.clone());
 
+    if let Err(ref e) = res {
+        eprintln!("validation happy path: {e}");
+    }
+
     assert!(res.is_ok(), "validation happy path");
 
     let res = execute(&mut ctx, &mut router, msg_envelope);
 
+    if let Err(ref e) = res {
+        eprintln!("execution happy path: {e}");
+    }
+
     assert!(res.is_ok(), "execution happy path");
 
-    // assert that the subject's client state is as expected
+    assert_eq!(
+        ctx.client_state(&msg.subject_client_id).unwrap(),
+        ctx.client_state(&msg.substitute_client_id).unwrap(),
+    )
 }
