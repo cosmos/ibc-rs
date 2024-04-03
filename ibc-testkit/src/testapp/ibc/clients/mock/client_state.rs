cf242303c3d74b2c7ffdbba0b7f2ff148f29003a
Sean Chen <seanchen11235@gmail.com>
2024-04-03 11:13:57 -0500

    Add docstring for `MockClientState::new`

--- ibc-testkit/src/testapp/ibc/clients/mock/client_state.rs
+++ ibc-testkit/src/testapp/ibc/clients/mock/client_state.rs
@@ -38,6 +38,9 @@ pub struct MockClientState {
 }
 
 impl MockClientState {
+    /// Initializes a new `MockClientState` with the given `MockHeader` and a
+    /// trusting period of 100 nanoseconds as a default. If the trusting period
+    /// needs to be changed, use the `with_trusting_period` method to override it.
     pub fn new(header: MockHeader) -> Self {
         Self {
             header,
