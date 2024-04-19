7d480535cf9d8e41e5129e580dc782065727839c
Adi Seredinschi <a@seredinschi.net>
2024-04-19 15:42:12 +0200

    Reproducing the bug with parsing PrefixedDenom
    refs:
    
    - https://x.com/zmagelinskas/status/1781280202582082003
    - https://gist.github.com/penso/9041c3ce4f7b7fe17b00ecf9ec3e1c8d

--- ibc-apps/ics20-transfer/types/src/denom.rs
+++ ibc-apps/ics20-transfer/types/src/denom.rs
@@ -399,6 +399,17 @@ mod tests {
     }
 
     #[test]
+    fn parse_denom() -> Result<(), TokenTransferError> {
+        let denom = "transfer/channel-75/factory/stars16da2uus9zrsy83h23ur42v3lglg5rmyrpqnju4/dust";
+        let dt = PrefixedDenom::from_str(denom)?;
+
+        assert_eq!(dt.base_denom.to_string(), "factory/stars16da2uus9zrsy83h23ur42v3lglg5rmyrpqnju4/dust");
+        assert_eq!(dt.trace_path.to_string(), "transfer/channel-75");
+
+        Ok(())
+    }
+
+    #[test]
     fn test_denom_trace() -> Result<(), TokenTransferError> {
         assert_eq!(
             PrefixedDenom::from_str("transfer/channel-0/uatom")?,
