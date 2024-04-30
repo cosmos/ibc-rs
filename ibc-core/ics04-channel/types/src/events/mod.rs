51453ab65d306418cc67b43d20a817d2b479d055
Rano | Ranadeep <ranadeep@informal.systems>
2024-04-30 17:57:56 +0200

    chore(deps): ibc-proto `0.44.0` and tendermint `0.36.0` (#1206)
    * update ibc-proto and tendemrint
    
    * compile tests
    
    * update cargo lockfiles
    
    * downgrade msrv to 1.71.1
    
    zeroize 1.8 with MSRV 1.72 is yanked
    
    * ignore patterns for depbot
    
    * changelog entry
    
    * ignore cw-storage-plus
    
    * use consistent cargo-toml
    
    * taplo for toml fmt
    
    * nit
    
    * Align cargo.toml entries
    
    * put multiline description in separate section
    
    * format toml
    
    * exclude tendermint cw contract from publishing
    
    ---------
    
    Signed-off-by: Rano | Ranadeep <ranadeep@informal.systems>
    Co-authored-by: Sean Chen <seanchen11235@gmail.com>

--- ibc-core/ics04-channel/types/src/events/mod.rs
+++ ibc-core/ics04-channel/types/src/events/mod.rs
@@ -1246,12 +1246,18 @@ mod tests {
             assert_eq!(t.kind, t.event.kind);
             assert_eq!(t.expected_keys.len(), t.event.attributes.len());
             for (i, e) in t.event.attributes.iter().enumerate() {
-                assert_eq!(e.key, t.expected_keys[i], "key mismatch for {:?}", t.kind);
+                assert_eq!(
+                    e.key_str().unwrap(),
+                    t.expected_keys[i],
+                    "key mismatch for {:?}",
+                    t.kind
+                );
             }
             assert_eq!(t.expected_values.len(), t.event.attributes.len());
             for (i, e) in t.event.attributes.iter().enumerate() {
                 assert_eq!(
-                    e.value, t.expected_values[i],
+                    e.value_str().unwrap(),
+                    t.expected_values[i],
                     "value mismatch for {:?}",
                     t.kind
                 );
