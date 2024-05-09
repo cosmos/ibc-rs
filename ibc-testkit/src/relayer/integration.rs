930f236905443dd8b1eb8c9a5fd04e33c5c37d2a
Ranadeep Biswas <mail@rnbguy.at>
2024-05-09 14:51:17 +0200

    use the new method

--- ibc-testkit/src/relayer/integration.rs
+++ ibc-testkit/src/relayer/integration.rs
@@ -1,20 +1,11 @@
-use core::str::FromStr;
-
-use ibc::apps::transfer::handler::send_transfer;
-use ibc::apps::transfer::types::msgs::transfer::MsgTransfer;
-use ibc::apps::transfer::types::packet::PacketData;
-use ibc::apps::transfer::types::PrefixedCoin;
-use ibc::core::channel::types::packet::Packet;
 use ibc::core::client::context::client_state::ClientStateValidation;
 use ibc::core::handler::types::events::IbcEvent;
 use ibc::core::host::types::identifiers::{ChannelId, ConnectionId, PortId};
-use ibc::primitives::Timestamp;
 
 use crate::context::TestContext;
 use crate::fixtures::core::signer::dummy_account_id;
 use crate::hosts::{HostClientState, TestHost};
 use crate::relayer::context::RelayerContext;
-use crate::testapp::ibc::applications::transfer::types::DummyTransferModule;
 use crate::testapp::ibc::core::types::DefaultIbcStore;
 
 /// Integration test for IBC implementation.
@@ -91,64 +82,12 @@ where
     assert_eq!(chan_id_on_a, ChannelId::new(1));
     assert_eq!(chan_id_on_b, ChannelId::new(1));
 
+    // ------------------------
     // send packet from A to B
+    // ------------------------
 
-    // generate packet for DummyTransferModule
-    let packet_data = PacketData {
-        token: PrefixedCoin::from_str("1000uibc").expect("valid prefixed coin"),
-        sender: signer.clone(),
-        receiver: signer.clone(),
-        memo: "sample memo".into(),
-    };
-
-    // packet with ibc metadata
-    // either height timeout or timestamp timeout must be set
-    let msg = MsgTransfer {
-        port_id_on_a: PortId::transfer(),
-        chan_id_on_a: chan_id_on_a.clone(),
-        packet_data,
-        // setting timeout height to 10 blocks from B's current height.
-        timeout_height_on_b: relayer.get_ctx_b().latest_height().add(10).into(),
-        // not setting timeout timestamp.
-        timeout_timestamp_on_b: Timestamp::none(),
-    };
-
-    // module creates the send_packet
-    send_transfer(
-        relayer.get_ctx_a_mut().ibc_store_mut(),
-        &mut DummyTransferModule,
-        msg,
-    )
-    .expect("successfully created send_packet");
-
-    // send_packet wasn't committed, hence produce a block
-    relayer.get_ctx_a_mut().advance_block_height();
-
-    // retrieve the send_packet event
-    let Some(IbcEvent::SendPacket(send_packet_event)) = relayer
-        .get_ctx_a()
-        .ibc_store()
-        .events
-        .lock()
-        .iter()
-        .rev()
-        .nth(2)
-        .cloned()
-    else {
-        panic!("unexpected event")
-    };
-
-    // create the IBC packet type
-    let packet = Packet {
-        port_id_on_a: send_packet_event.port_id_on_a().clone(),
-        chan_id_on_a: send_packet_event.chan_id_on_a().clone(),
-        seq_on_a: *send_packet_event.seq_on_a(),
-        data: send_packet_event.packet_data().to_vec(),
-        timeout_height_on_b: *send_packet_event.timeout_height_on_b(),
-        timeout_timestamp_on_b: *send_packet_event.timeout_timestamp_on_b(),
-        port_id_on_b: send_packet_event.port_id_on_b().clone(),
-        chan_id_on_b: send_packet_event.chan_id_on_b().clone(),
-    };
+    let packet =
+        relayer.send_packet_via_dummy_transfer_module_on_a(chan_id_on_a.clone(), signer.clone());
 
     // continue packet relay starting from recv_packet at B
     relayer.send_packet_on_a(packet, signer.clone());
@@ -165,64 +104,12 @@ where
         panic!("unexpected event")
     };
 
+    // --------------------------
     // timeout packet from A to B
+    // --------------------------
 
-    // generate packet for DummyTransferModule
-    let packet_data = PacketData {
-        token: PrefixedCoin::from_str("1000uibc").expect("valid prefixed coin"),
-        sender: signer.clone(),
-        receiver: signer.clone(),
-        memo: "sample memo".into(),
-    };
-
-    // packet with ibc metadata
-    // either height timeout or timestamp timeout must be set
-    let msg = MsgTransfer {
-        port_id_on_a: PortId::transfer(),
-        chan_id_on_a: chan_id_on_a.clone(),
-        packet_data,
-        // setting timeout height to 10 blocks from B's current height.
-        timeout_height_on_b: relayer.get_ctx_b().latest_height().add(10).into(),
-        // not setting timeout timestamp.
-        timeout_timestamp_on_b: Timestamp::none(),
-    };
-
-    // module creates the send_packet
-    send_transfer(
-        relayer.get_ctx_a_mut().ibc_store_mut(),
-        &mut DummyTransferModule,
-        msg,
-    )
-    .expect("successfully created send_packet");
-
-    // send_packet wasn't committed, hence produce a block
-    relayer.get_ctx_a_mut().advance_block_height();
-
-    // retrieve the send_packet event
-    let Some(IbcEvent::SendPacket(send_packet_event)) = relayer
-        .get_ctx_a()
-        .ibc_store()
-        .events
-        .lock()
-        .iter()
-        .rev()
-        .nth(2)
-        .cloned()
-    else {
-        panic!("unexpected event")
-    };
-
-    // create the IBC packet type
-    let packet = Packet {
-        port_id_on_a: send_packet_event.port_id_on_a().clone(),
-        chan_id_on_a: send_packet_event.chan_id_on_a().clone(),
-        seq_on_a: *send_packet_event.seq_on_a(),
-        data: send_packet_event.packet_data().to_vec(),
-        timeout_height_on_b: *send_packet_event.timeout_height_on_b(),
-        timeout_timestamp_on_b: *send_packet_event.timeout_timestamp_on_b(),
-        port_id_on_b: send_packet_event.port_id_on_b().clone(),
-        chan_id_on_b: send_packet_event.chan_id_on_b().clone(),
-    };
+    let packet =
+        relayer.send_packet_via_dummy_transfer_module_on_a(chan_id_on_a.clone(), signer.clone());
 
     // timeout the packet on A; never relay the packet to B
     relayer.timeout_packet_on_a(packet.clone(), signer.clone());
@@ -239,64 +126,12 @@ where
         panic!("unexpected event")
     };
 
-    // timeout packet from A to B; using close channel
+    // ------------------------------------------------
+    // timeout packet from A to B; using closed channel
+    // ------------------------------------------------
 
-    // generate packet for DummyTransferModule
-    let packet_data = PacketData {
-        token: PrefixedCoin::from_str("1000uibc").expect("valid prefixed coin"),
-        sender: signer.clone(),
-        receiver: signer.clone(),
-        memo: "sample memo".into(),
-    };
-
-    // packet with ibc metadata
-    // either height timeout or timestamp timeout must be set
-    let msg = MsgTransfer {
-        port_id_on_a: PortId::transfer(),
-        chan_id_on_a: chan_id_on_a.clone(),
-        packet_data,
-        // setting timeout height to 10 blocks from B's current height.
-        timeout_height_on_b: relayer.get_ctx_b().latest_height().add(10).into(),
-        // not setting timeout timestamp.
-        timeout_timestamp_on_b: Timestamp::none(),
-    };
-
-    // module creates the send_packet
-    send_transfer(
-        relayer.get_ctx_a_mut().ibc_store_mut(),
-        &mut DummyTransferModule,
-        msg,
-    )
-    .expect("successfully created send_packet");
-
-    // send_packet wasn't committed, hence produce a block
-    relayer.get_ctx_a_mut().advance_block_height();
-
-    // retrieve the send_packet event
-    let Some(IbcEvent::SendPacket(send_packet_event)) = relayer
-        .get_ctx_a()
-        .ibc_store()
-        .events
-        .lock()
-        .iter()
-        .rev()
-        .nth(2)
-        .cloned()
-    else {
-        panic!("unexpected event")
-    };
-
-    // create the IBC packet type
-    let packet = Packet {
-        port_id_on_a: send_packet_event.port_id_on_a().clone(),
-        chan_id_on_a: send_packet_event.chan_id_on_a().clone(),
-        seq_on_a: *send_packet_event.seq_on_a(),
-        data: send_packet_event.packet_data().to_vec(),
-        timeout_height_on_b: *send_packet_event.timeout_height_on_b(),
-        timeout_timestamp_on_b: *send_packet_event.timeout_timestamp_on_b(),
-        port_id_on_b: send_packet_event.port_id_on_b().clone(),
-        chan_id_on_b: send_packet_event.chan_id_on_b().clone(),
-    };
+    let packet =
+        relayer.send_packet_via_dummy_transfer_module_on_a(chan_id_on_a.clone(), signer.clone());
 
     // timeout the packet on A; never relay the packet to B
     relayer.timeout_packet_on_channel_close_on_a(packet.clone(), signer.clone());
