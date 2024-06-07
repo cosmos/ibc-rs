a18a461e5f47810086292c4697bce87bd34e35db
Ranadeep Biswas <mail@rnbguy.at>
2024-06-07 10:07:21 +0200

    add pack_host_consensus_state fn

--- ibc-core/ics03-connection/src/handler/mod.rs
+++ ibc-core/ics03-connection/src/handler/mod.rs
@@ -53,3 +53,37 @@ where
         Ok(CS::try_from(value).map_err(Into::<ClientError>::into)?)
     }
 }
+
+/// Pack the host consensus state in the expected format stored at the counterparty chain.
+///
+/// Currently, the IBC-go enabled chains stores Wasm LightClient states in a WasmConsensusState
+/// wrapper. This function packs the consensus state in the WasmConsensusState wrapper
+/// if the client identifier at counterparty is of Wasm client type.
+pub(crate) fn pack_host_consensus_state<CS>(
+    value: CS,
+    host_client_id_at_counterparty: &ClientId,
+) -> Any
+where
+    CS: Into<Any>,
+{
+    let any_value = value.into();
+
+    #[cfg(feature = "wasm-client")]
+    if host_client_id_at_counterparty.is_wasm_client_id() {
+        use ibc_client_wasm_types::consensus_state::ConsensusState as WasmConsensusState;
+        use prost::Message;
+
+        let wasm_consensus_state = WasmConsensusState::new(any_value.encode_to_vec());
+
+        wasm_consensus_state.into()
+    } else {
+        any_value
+    }
+
+    #[cfg(not(feature = "wasm-client"))]
+    {
+        // this avoids lint warning for unused variable.
+        let _ = host_client_id_at_counterparty;
+        any_value
+    }
+}
