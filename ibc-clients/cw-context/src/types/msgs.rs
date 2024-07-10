b61bf545763dc4c5dd598e7de2c85b3addd94d16
Farhad Shabani <farhad.shabani@gmail.com>
2024-07-10 12:09:52 -0700

    feat: allow empty CommitmentPrefix

--- ibc-clients/cw-context/src/types/msgs.rs
+++ ibc-clients/cw-context/src/types/msgs.rs
@@ -177,7 +177,7 @@ impl TryFrom<VerifyMembershipMsgRaw> for VerifyMembershipMsg {
             path,
             value: raw.value,
             height,
-            prefix: CommitmentPrefix::try_from(prefix)?,
+            prefix: CommitmentPrefix::from(prefix),
             delay_block_period: raw.delay_block_period,
             delay_time_period: raw.delay_time_period,
         })
@@ -217,7 +217,7 @@ impl TryFrom<VerifyNonMembershipMsgRaw> for VerifyNonMembershipMsg {
             proof,
             path,
             height,
-            prefix: CommitmentPrefix::try_from(prefix)?,
+            prefix: CommitmentPrefix::from(prefix),
             delay_block_period: raw.delay_block_period,
             delay_time_period: raw.delay_time_period,
         })
