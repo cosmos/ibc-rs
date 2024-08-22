123710e5a02ed7029c658bed4fc30753691dca82
Sean Chen <seanchen11235@gmail.com>
2024-08-22 12:34:16 -0500

    Remove unnecessary PacketError variant

--- ibc-core/ics04-channel/types/src/error.rs
+++ ibc-core/ics04-channel/types/src/error.rs
@@ -108,8 +108,6 @@ pub enum PacketError {
     InvalidTimeoutTimestamp(TimestampError),
     /// invalid identifier: `{0}`
     InvalidIdentifier(IdentifierError),
-    /// invalid path prefix: `{0}`
-    InvalidPathPrefix(String),
     /// empty acknowledgment not allowed
     EmptyAcknowledgment,
     /// empty acknowledgment status not allowed
