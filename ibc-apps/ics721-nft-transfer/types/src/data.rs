8a7074a21f9506b23dcf18e0895102e9926b9b68
Sean Chen <seanchen11235@gmail.com>
2024-08-28 13:43:52 -0500

    Add serde feature attribute

--- ibc-apps/ics721-nft-transfer/types/src/data.rs
+++ ibc-apps/ics721-nft-transfer/types/src/data.rs
@@ -6,6 +6,7 @@ use core::str::FromStr;
 use base64::prelude::BASE64_STANDARD;
 #[cfg(feature = "serde")]
 use base64::Engine;
+#[cfg(feature = "serde")]
 use ibc_core::host::types::error::DecodingError;
 use ibc_core::primitives::prelude::*;
 use mime::Mime;
