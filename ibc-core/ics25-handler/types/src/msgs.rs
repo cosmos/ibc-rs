77b42b1e55254e0f9432c3dc941b6a15ab9ee3e1
Sean Chen <seanchen11235@gmail.com>
2024-08-29 15:42:40 -0500

    Remove RouterError::UnknownMessageTypeUrl

--- ibc-core/ics25-handler/types/src/msgs.rs
+++ ibc-core/ics25-handler/types/src/msgs.rs
@@ -46,45 +46,45 @@ impl TryFrom<Any> for MsgEnvelope {
             // ICS2 messages
             CREATE_CLIENT_TYPE_URL => {
                 // Pop out the message and then wrap it in the corresponding type.
-                let domain_msg = MsgCreateClient::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                let domain_msg =
+                    MsgCreateClient::decode_vec(&any_msg.value).map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Client(ClientMsg::CreateClient(domain_msg)))
             }
             UPDATE_CLIENT_TYPE_URL => {
-                let domain_msg = MsgUpdateClient::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                let domain_msg =
+                    MsgUpdateClient::decode_vec(&any_msg.value).map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Client(ClientMsg::UpdateClient(domain_msg)))
             }
             UPGRADE_CLIENT_TYPE_URL => {
                 let domain_msg = MsgUpgradeClient::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Client(ClientMsg::UpgradeClient(domain_msg)))
             }
             SUBMIT_MISBEHAVIOUR_TYPE_URL => {
                 let domain_msg = MsgSubmitMisbehaviour::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Client(ClientMsg::Misbehaviour(domain_msg)))
             }
 
             // ICS03
             CONN_OPEN_INIT_TYPE_URL => {
                 let domain_msg = MsgConnectionOpenInit::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Connection(ConnectionMsg::OpenInit(domain_msg)))
             }
             CONN_OPEN_TRY_TYPE_URL => {
                 let domain_msg = MsgConnectionOpenTry::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Connection(ConnectionMsg::OpenTry(domain_msg)))
             }
             CONN_OPEN_ACK_TYPE_URL => {
                 let domain_msg = MsgConnectionOpenAck::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Connection(ConnectionMsg::OpenAck(domain_msg)))
             }
             CONN_OPEN_CONFIRM_TYPE_URL => {
                 let domain_msg = MsgConnectionOpenConfirm::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Connection(ConnectionMsg::OpenConfirm(
                     domain_msg,
                 )))
@@ -93,56 +93,57 @@ impl TryFrom<Any> for MsgEnvelope {
             // ICS04 channel messages
             CHAN_OPEN_INIT_TYPE_URL => {
                 let domain_msg = MsgChannelOpenInit::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Channel(ChannelMsg::OpenInit(domain_msg)))
             }
             CHAN_OPEN_TRY_TYPE_URL => {
                 let domain_msg = MsgChannelOpenTry::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Channel(ChannelMsg::OpenTry(domain_msg)))
             }
             CHAN_OPEN_ACK_TYPE_URL => {
                 let domain_msg = MsgChannelOpenAck::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Channel(ChannelMsg::OpenAck(domain_msg)))
             }
             CHAN_OPEN_CONFIRM_TYPE_URL => {
                 let domain_msg = MsgChannelOpenConfirm::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Channel(ChannelMsg::OpenConfirm(domain_msg)))
             }
             CHAN_CLOSE_INIT_TYPE_URL => {
                 let domain_msg = MsgChannelCloseInit::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Channel(ChannelMsg::CloseInit(domain_msg)))
             }
             CHAN_CLOSE_CONFIRM_TYPE_URL => {
                 let domain_msg = MsgChannelCloseConfirm::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Channel(ChannelMsg::CloseConfirm(domain_msg)))
             }
             // ICS04 packet messages
             RECV_PACKET_TYPE_URL => {
-                let domain_msg = MsgRecvPacket::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                let domain_msg =
+                    MsgRecvPacket::decode_vec(&any_msg.value).map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Packet(PacketMsg::Recv(domain_msg)))
             }
             ACKNOWLEDGEMENT_TYPE_URL => {
                 let domain_msg = MsgAcknowledgement::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Packet(PacketMsg::Ack(domain_msg)))
             }
             TIMEOUT_TYPE_URL => {
-                let domain_msg = MsgTimeout::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                let domain_msg =
+                    MsgTimeout::decode_vec(&any_msg.value).map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Packet(PacketMsg::Timeout(domain_msg)))
             }
             TIMEOUT_ON_CLOSE_TYPE_URL => {
                 let domain_msg = MsgTimeoutOnClose::decode_vec(&any_msg.value)
-                    .map_err(|e| RouterError::Decoding(DecodingError::Protobuf(e)))?;
+                    .map_err(DecodingError::Protobuf)?;
                 Ok(MsgEnvelope::Packet(PacketMsg::TimeoutOnClose(domain_msg)))
             }
-            _ => Err(RouterError::UnknownMessageTypeUrl(any_msg.type_url)),
+
+            _ => Err(DecodingError::UnknownTypeUrl(any_msg.type_url))?,
         }
     }
 }
