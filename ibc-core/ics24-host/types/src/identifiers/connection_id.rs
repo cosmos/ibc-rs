e85fa257b9af6eb0b9279e7d5a74cb6e38c2a823
Ranadeep Biswas <mail@rnbguy.at>
2024-10-03 17:26:08 +0200

    refactor v2 connection id

--- ibc-core/ics24-host/types/src/identifiers/connection_id.rs
+++ ibc-core/ics24-host/types/src/identifiers/connection_id.rs
@@ -1,10 +1,10 @@
 use core::fmt::{Display, Error as FmtError, Formatter};
 use core::str::FromStr;
 
-use derive_more::Into;
 use ibc_primitives::prelude::*;
 
 use crate::error::IdentifierError;
+use crate::identifiers::ClientId;
 use crate::validate::validate_connection_identifier;
 
 const CONNECTION_ID_PREFIX: &str = "connection";
@@ -23,8 +23,11 @@ const CONNECTION_ID_PREFIX: &str = "connection";
 )]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "schema", derive(schemars::JsonSchema))]
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Into)]
-pub struct ConnectionId(String);
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum ConnectionId {
+    V1(String),
+    V2(ClientId),
+}
 
 impl ConnectionId {
     /// Builds a new connection identifier. Connection identifiers are deterministically formed from
@@ -39,7 +42,7 @@ impl ConnectionId {
     /// ```
     pub fn new(identifier: u64) -> Self {
         let id = format!("{}-{}", Self::prefix(), identifier);
-        Self(id)
+        Self::V1(id)
     }
 
     /// Returns the static prefix to be used across all connection identifiers.
@@ -49,12 +52,18 @@ impl ConnectionId {
 
     /// Get this identifier as a borrowed `&str`
     pub fn as_str(&self) -> &str {
-        &self.0
+        match self {
+            Self::V1(id) => id.as_str(),
+            Self::V2(id) => id.as_str(),
+        }
     }
 
     /// Get this identifier as a borrowed byte slice
     pub fn as_bytes(&self) -> &[u8] {
-        self.0.as_bytes()
+        match self {
+            Self::V1(id) => id.as_bytes(),
+            Self::V2(id) => id.as_bytes(),
+        }
     }
 
     /// Return ConnectionId with identifier 0
@@ -66,7 +75,10 @@ impl ConnectionId {
 /// This implementation provides a `to_string` method.
 impl Display for ConnectionId {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
-        write!(f, "{}", self.0)
+        match self {
+            Self::V1(id) => write!(f, "{id}"),
+            Self::V2(id) => write!(f, "{id}"),
+        }
     }
 }
 
@@ -74,7 +86,9 @@ impl FromStr for ConnectionId {
     type Err = IdentifierError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        validate_connection_identifier(s).map(|_| Self(s.to_string()))
+        validate_connection_identifier(s)
+            .map(|_| Self::V1(s.to_string()))
+            .or_else(|_| Ok(Self::V2(s.parse()?)))
     }
 }
 
@@ -89,5 +103,36 @@ impl FromStr for ConnectionId {
 impl PartialEq<str> for ConnectionId {
     fn eq(&self, other: &str) -> bool {
         self.as_str().eq(other)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::string::ToString;
+    use core::str::FromStr;
+
+    use super::ConnectionId;
+
+    #[test]
+    fn test_channel_id() {
+        let id = ConnectionId::new(27);
+        assert_eq!(id.to_string(), "connection-27");
+        let id2 = ConnectionId::from_str("connection-27").unwrap();
+        assert_eq!(id, id2);
+        assert!(matches!(id, ConnectionId::V1(_)));
+    }
+
+    #[test]
+    fn test_channel_id_from_client_id() {
+        let channel_id_str = "07-tendermint-21";
+        let channel_id = ConnectionId::from_str(channel_id_str).unwrap();
+        assert!(matches!(channel_id, ConnectionId::V2(_)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_channel_id_from_client_id_fail() {
+        let channel_id_str = "connectionToA";
+        let _ = ConnectionId::from_str(channel_id_str).unwrap();
     }
 }
