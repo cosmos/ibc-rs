d1a17ddbf6ac75fb218049e32cc18318f9d38f35
Ranadeep Biswas <mail@rnbguy.at>
2024-10-23 17:32:42 +0200

    use From derive for ChannelId

--- ibc-eureka-core/ics24-host/types/src/identifiers/channel_id.rs
+++ ibc-eureka-core/ics24-host/types/src/identifiers/channel_id.rs
@@ -1,7 +1,7 @@
 use core::fmt::{Debug, Display, Error as FmtError, Formatter};
 use core::str::FromStr;
 
-use derive_more::Into;
+use derive_more::{From, Into};
 use ibc_primitives::prelude::*;
 
 use crate::error::IdentifierError;
@@ -23,14 +23,8 @@ const CHANNEL_ID_PREFIX: &str = "channel";
 )]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "schema", derive(schemars::JsonSchema))]
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Into)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, From, Into)]
 pub struct ChannelId(ClientId);
-
-impl From<ClientId> for ChannelId {
-    fn from(client_id: ClientId) -> Self {
-        Self(client_id)
-    }
-}
 
 impl ChannelId {
     /// Returns the static prefix to be used across all channel identifiers.
